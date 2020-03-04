abdf0e2919bc53b7e6903dd0aaffbaed4ace3527
firak01 <firak01@t-online.de>
2020-03-04 17:54:45 +0100

    1
    Signed-off-by: firak01 <firak01@t-online.de>

--- OpenVPNZZZ/src/use/openvpn/AbstractConfigStarterOVPN.java
+++ OpenVPNZZZ/src/use/openvpn/AbstractConfigStarterOVPN.java
@@ -299,16 +299,22 @@ public abstract class AbstractConfigStarterOVPN extends KernelUseObjectZZZ imple
 			//ServerConfigMapper4BatchOVPN objMapperBatch = new ServerConfigMapper4BatchOVPN(this.getKernelObject(), this.getServerObject(), fileConfigTemplateBatch);
 			IConfigMapper4BatchOVPN objMapperBatch = this.getConfigMapperObject(); //new ServerConfigMapper4BatchOVPN(this.getKernelObject(), this.getServerObject(), fileConfigTemplateBatch);
 			HashMapIterableKeyZZZ<String, String>hmBatchLines = objMapperBatch.readTaskHashMap();								
-			Set<String> setBatchLineNumber = hmBatchLines.keySet();
 			
 			//111111111 TODO GOON 20200304: Mal sehen was die IterableKey HashMap leistet...
 			//Aber: Die Sortierung ist im Set nicht sichergestellt. Darum explizit sortieren.
-			//List<String>numbersList=(List<String>) SetZZZ.sortAsString(setBatchLineNumber);			
-			List<String>numbersList=(List<String>) SetZZZ.sortAsInteger(setBatchLineNumber);
-			for(String sLineNumber : numbersList) {
-				String sLine = hmBatchLines.get(sLineNumber);
+			//List<String>numbersList=(List<String>) SetZZZ.sortAsString(setBatchLineNumber);
+//			Set<String> setBatchLineNumber = hmBatchLines.keySet();			
+//			List<String>numbersList=(List<String>) SetZZZ.sortAsInteger(setBatchLineNumber);
+//			for(String sLineNumber : numbersList) {
+//				String sLine = hmBatchLines.get(sLineNumber);
+//				listasReturn.add(sLine);
+//			}	
+			
+			for(String sKey : hmBatchLines) {
+				String sLine = hmBatchLines.getValue(sKey);
 				listasReturn.add(sLine);
-			}			
+			}
+			
 		}
 		return listasReturn;
 	}	
